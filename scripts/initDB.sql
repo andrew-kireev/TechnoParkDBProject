522a2dfda42e90f0c413f62bb4e3f1f0ac504a81
Andrew <a-kireev1989@mail.ru>
2021-06-05 23:08:25 +0300

    Индексы

--- scripts/initDB.sql
+++ scripts/initDB.sql
@@ -186,15 +186,15 @@ create index if not exists thread_user on threads using hash (author);
 create index if not exists thread_full on threads (slug, id, forum);
 
 create index if not exists post_pathparent on posts ((path[1]));
-create index if not exists posts_forum on posts using hash (forum);
-create index if not exists posts_author on posts using hash (author);
+-- create index if not exists posts_forum on posts using hash (forum);
+-- create index if not exists posts_author on posts using hash (author);
 create index if not exists posts_thread_thread_id on posts (thread, id);
 CREATE INDEX IF NOT EXISTS post_path_id on posts (id, (path[1]));
 create index if not exists post_parent on posts (thread, id, (path[1]), parent);
 create index if not exists posts_sorting on posts using gin ((path[1]) desc, path, id);
 create index if not exists post_thread on posts (thread);
 create index if not exists post_gin on posts using gin (path);
-create index if not exists posts_thread_path_id on posts (thread, path, id);
+-- create index if not exists posts_thread_path_id on posts (thread, path, id);
 
 create index if not exists users_nickname_hash on users using hash (nickname);
 -- create index if not exists users_email_hash on users using hash (email);
