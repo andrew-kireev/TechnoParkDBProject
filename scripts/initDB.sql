c53d61476b3dc04ef24dba50fc40f013e9fef17c
Andrew <a-kireev1989@mail.ru>
2021-06-05 20:32:11 +0300

    Индексы

--- scripts/initDB.sql
+++ scripts/initDB.sql
@@ -186,8 +186,8 @@ create index if not exists thread_user on threads using hash (author);
 create index if not exists thread_full on threads (slug, id, forum);
 
 create index if not exists post_pathparent on posts ((path[1]));
--- create index if not exists posts_forum on posts using hash (forum);
--- create index if not exists posts_author on posts using hash (author);
+create index if not exists posts_forum on posts using hash (forum);
+create index if not exists posts_author on posts using hash (author);
 create index if not exists posts_thread_thread_id on posts (thread, id);
 CREATE INDEX IF NOT EXISTS post_path_id on posts (id, (path[1]));
 create index if not exists post_parent on posts (thread, id, (path[1]), parent);
