96108e3fda1a3d600ef6bcad077703908ba95bf4
Andrew Kireev <a-kireev1989@mail.ru>
2021-06-07 09:28:42 +0300

    fdsfds

--- scripts/initDB.sql
+++ scripts/initDB.sql
@@ -183,12 +183,10 @@ create index if not exists thread_slug_hash on threads using hash (slug);
 create index if not exists thread_forum_created on threads (forum, created);
 create index if not exists thread_user on threads using hash (author);
 create index if not exists thread_full on threads (slug, id, forum);
-create index if not exists thread_all on threads (title, author, forum, message, votes, slug, created);
 
-create index if not exists post_pathparent on posts ((path[1]));
+
 create index if not exists posts_thread_thread_id on posts (thread, id);
-CREATE INDEX IF NOT EXISTS post_path_id on posts (id, (path[1]));
-create index if not exists post_parent on posts (thread, id, (path[1]), parent);
+create index if not exists post_path_id on posts (id, (path[1]));
 create index if not exists posts_sorting on posts using gin ((path[1]) desc, path, id);
 create index if not exists post_thread on posts (thread);
 create index if not exists post_gin on posts using gin (path);
